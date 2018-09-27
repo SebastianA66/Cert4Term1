e8550a9af927dbf40e09b53b02ed91df73ec57a9
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-09-27 12:08:33 +1000

    Initial commit

---
+++ Assets/My work/Scripts/Towers/Placement/TowerPlacement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TowerDefense.Towers.Placement;
+
+
+public class TowerPlacement : MonoBehaviour
+{
+
+    public LayerMask hitLayers;
+    
+
+    private void OnDrawGizmos()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(camRay.origin, camRay.origin + camRay.direction * 1000f);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Perform raycaast
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(camRay, out hit, 1000f, hitLayers))
+            {
+                //Check if hitting grid
+                IPlacementArea placement = hit.collider.GetComponent<IPlacementArea>();
+                //Get grid position
+                if (placement != null)
+                {
+                    //snap position tower to grid element
+                    transform.position = placement.Snap(hit.point, new Core.Utilities.IntVector2(1, 1));
+                }
+            }
+        }
+        
+
+
+
+    }
+}
